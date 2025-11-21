a78cfde1d5865e5e9c1c57eb543a5b42532373e4
Dhanraj Pawar <dhanraj2982003@gmail.com>
2025-11-21 23:24:46 +0530

    optimized way to check Armstrong Number or Not

---
+++ ArmstrongNumCheck.java
@@ -0,0 +1,25 @@
+package Java_Daily_Practic.Day1;
+
+import java.util.Scanner;
+
+public class ArmstrongNumCheck {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number to check:");
+        int num=sc.nextInt();
+        int orgNum=num;
+        int count=String.valueOf(num).length();
+        int sum=0;
+
+        while(num>0)
+        {
+            int rem=num%10;
+            sum+=Math.pow(rem,count);
+            num/=10;
+        }
+
+        String result=(orgNum==sum?"Given Number is Armstrong!":"Given Number is Not Armstrong!");
+        System.out.println(result);
+    }
+}
