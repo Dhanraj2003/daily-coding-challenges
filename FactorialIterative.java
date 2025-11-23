d07248c1b16ce1628afe4a34cff0be1ae7134fc1
Dhanraj Pawar <dhanraj2982003@gmail.com>
2025-11-23 17:50:41 +0530

    solve two problems

---
+++ FactorialIterative.java
@@ -0,0 +1,22 @@
+package Java_Daily_Practic.Day1;
+
+import java.util.Scanner;
+
+public class FactorialIterative {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + n + " is: " + factorial);
+    }
+}
